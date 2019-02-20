898344d90d0940a4138f37d8fcd544bdf39ecd4f
IsSomthingWrong <43751371+IsSomthingWrong@users.noreply.github.com>
2019-02-20 09:33:26 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,575 @@
+print ("_    _ ||==== ||       __     __   _    _ ||==== ")
+print ("|    | ||     ||     //  \\\\  /  \  |\  /| ||     ")
+print ("| || | ||___  ||     ||     / -- \ |\\\\//| ||___  ")
+print ("| || | ||     ||     ||     | || | | || | ||     ")
+print ("| /\ | ||     ||     ||     \ -- / | || | ||     ")
+print ("|//\\\\| ||==== \\\\==== \\\\__//  \__/  | || | ||==== ")
+test = 0
+text = "Press Enter"
+v1 = 0
+while 1>test: 
+    user = input (text)
+
+
+
+    output = ["","","","","",""]
+
+    space = ['       ', '       ', '       ', '       ', '       ', '       ']
+
+    s1 = ['  ||   ','  ||   ','  ||   ','  \/   ','       ','  []   ']
+
+    s2 = [' //--\\\\ ', '// //|| ', '|| |||| ', '\\\\ \\\\|| ', ' \\\\___  ', '  `---" ']
+
+    s3 = [ '        ', ' || ||  ', '=||=||= ', ' || ||  ', '=||=||= ', ' || ||  ']
+
+    s4 = ['//||\\\\  ', '\\\\||    ', ' \\||-   ', '  -||\\  ', '   ||\\\\ ', ' \\\\||// ']
+
+    s5 = ['/\   // ', '\/  //  ', '   //   ', '  //    ', ' //  /\ ', '//   \/ ']
+
+    s6 = ['        ', '        ', '   _    ', '  /^\   ', ' // \\\\  ', '//   \\\\ ']
+
+    s7 = ['        ', ' //\\\\   ', ' \\\\//   ', ' //\\\\ | ', '||  \\\\/ ', ' \\\\//\\\\ ']
+
+    s8 = ['       ', '  _|_  ', '  / \  ', '       ', '       ', '       ']
+
+    s9 = [' //===] ', '//      ', '||      ', '||      ', '\\\\      ', ' \\\\===] ']
+
+    s0 = ['[===\\\\  ', '     \\\\ ', '     || ', '     || ', '     // ', '[===//  ']
+  
+    s11 = ['  \\\\   ', '   \\\\  ', '   --  ', '       ', '       ', '       ']
+
+    s12 = ['        ', '        ', ' _    _ ', '//``=// ', '        ', '        ']
+
+    s13 = ['        ', '  ____  ', ' [____] ', '        ', '        ', '        ']
+
+    s14 = ['       ', '       ', '       ', '       ', '       ', '=======']
+
+    s15 = ['       ', '       ', ' _____ ', ' _____ ', '       ', '       ']
+
+    s16 = ['       ', '   |   ', ' --+-- ', '   |   ', '       ', '       ']
+
+    s17 = ['||====] ', '||      ', '||      ', '||      ', '||      ', '||====] ']
+
+    s18 = [' /===]  ', ' |      ', '//      ', '\\\\      ', ' |      ', ' \\===]  ']
+
+    s19 = ['[====|| ', '     || ', '     || ', '     || ', '     || ', '[====|| ']
+
+    s20 = [' [===\\ ', '     |  ', '     \\\\ ', '     // ', '     |  ', ' [===/ ']
+
+    s21 = ['\\\\      ', ' \\\\     ', '  \\\\    ', '   \\\\   ', '    \\\\  ', '     \\\\ ']
+
+    s22 = ['   |   ', '   |   ', '   |   ', '   |   ', '   |   ', '   |   ']
+
+    s23 = ['   /\\  ', '   \\/  ', '       ', '       ', '   \\\\  ', '   //  ']
+
+    s24 = ['   /\\  ', '   \\/  ', '       ', '       ', '   /\\  ', '   \\/  ']
+
+    s25 = ['   \\\\  ', '   //  ', '       ', '       ', '       ', '       ']
+
+    s26 = [' \\\\ \\\\ ', ' // // ', '       ', '       ', '       ', '       ']
+
+    s27 = ['       ', '       ', '       ', '       ', '   \\\\  ', '   //  ']
+
+    s28 = ['     // ', '   //   ', ' //     ', ' \\\\     ', '   \\\\   ', '     \\\\ ']
+
+    s29 = ['       ', '       ', '       ', '       ', '   /\\  ', '   \\/  ']
+
+    s30 = ['\\\\      ', '  \\\\    ', '    \\\\  ', '    //  ', '  //    ', '//      ']
+
+    s31 = ['     //', '    // ', '   //  ', '  //   ', ' //    ', '//     ']
+
+    s32 = [' //=\\\\  ', '     || ', '    //  ', '   ||   ', '        ', '   []   ']
+
+    A = [' ____  ', '//  \\\\ ', '||  || ', '||  || ', '||==|| ', '||  || ']
+
+    B = ['||=\\\\  ', '||  || ', '||__// ', '||  \\\\ ', '||  || ', '||=//  ']
+
+    C = ['  __   ', '//  \\\\ ', '||     ', '||     ', '||     ', '\\\\__// ']
+
+    D = ['____   ', '||  \\\\ ', '||  || ', '||  || ', '||  || ', '||__// ']
+
+    E = ['||==== ', '||     ', '||__   ', '||     ', '||     ', '||==== ']
+
+    F = ['||==== ', '||     ', '||__   ', '||     ', '||     ', '||     ']
+
+    G = ['  __   ', '//  \\\\ ', '||     ', '|| ___ ', '||  \\\\ ', '\\\\__// ']
+  
+    H = ['||  || ', '||__|| ', '||  || ', '||  || ', '||  || ', '||  || ']
+
+    I = ['====== ', '  ||   ', '  ||   ', '  ||   ', '  ||   ', '====== ']
+
+    J = ['====== ', '   ||  ', '   ||  ', '   ||  ', '   //  ', '\\\\//   ']
+
+    K = ['||  // ', '|| //  ', '||//   ', '||\\\\   ', '|| \\\\  ', '||  \\\\ ']
+
+    L = ['||     ', '||     ', '||     ', '||     ', '||     ', '\\\\==== ']
+
+    M = ['_    _ ', '|\  /| ', '|\\\\//| ', '| || | ', '| || | ', '| || | ']
+
+    N = ['|    | ', '|\   | ', '| \  | ', '|  \ | ', '|   \| ', '|    | ']
+
+    O = ['  __   ', ' /  \  ', '/ -- \\ ', '| || | ', '\ -- / ', ' \__/  ']
+
+    P = ['||===\\\\ ', '||   || ', '||===// ', '||      ', '||      ', '||      ']
+
+    Q = ['  __   ', ' /  \  ', '/ -- \\ ', '| || | ', '\ --\\\\ ', ' \__/\\ ']
+
+    R = ['||===\\\\ ', '||   || ', '||===// ', '||  \\\\  ', '||   \\\\ ', '||   || ']
+
+    S = [' ----- ', '//     ', '\\\\___  ', '    \\\\ ', '    // ', '-----  ']
+
+    T = ['====== ', '  ||   ', '  ||   ', '  ||   ', '  ||   ', '  ||   ']
+
+    U = ['||  || ', '||  || ', '||  || ', '||  || ', '||__|| ', '\\\\__// ']
+
+    V = ['__  __ ', '||  || ', '\\\\  // ', ' \\\\//  ', '  \/   ', '   `   ']
+
+    W = ['_    _ ', '|    | ', '| || | ', '| || | ', '| /\ | ', '|//\\\\| ']
+
+    X = ['_    _ ', '\\\\  // ', ' \\\\//  ', ' //\\\\  ', '//  \\\\ ', '-    - ']
+
+    Y = ['_    _ ', '\\\\  // ', ' \\\\//  ', '  ||   ', '  ||   ', '  ||   ']
+
+    Z = ['====== ', '    // ', '   //  ', '  //   ', ' //    ', '====== ']
+
+    one = [' __    ', '//||   ', '  ||   ', '  ||   ', '  ||   ', ' ====  ']
+
+
+    two = [' ___   ', '// \\\\  ', '   ||  ', '  //   ', ' ||    ', ' ===== ']
+
+
+    three = [' ___   ', '// \\\\  ', '   //  ', '  =||  ', '   \\\\  ', '\\\\_//  ']
+
+
+    four = ['       ', '||  || ', '\\\\__|| ', '    || ', '    || ', '    || ']
+
+
+    five = ['====== ', '||     ', '\\\\__   ', '    \\\\ ', '\\\\__// ', '       ']
+
+
+    six = [' ___   ', '// \\\\  ', '|| __  ', '||/  \\ ', '|| ()| ', ' \\\\__/ ']
+
+
+    seven = ['====== ', '\\\\     ', ' \\\\    ', '  \\\\   ', '  ||   ', '  ||   ']
+
+
+    eight = [' ____  ', '/ __ \\ ', '\\ \/ / ', '/ __ \\ ', '| \/ | ', '\\____/ ']
+
+
+    nine = ['/==\\\\  ', '|() || ', '\__/|| ', '    || ', '    || ', '    || ']
+
+
+    zero = [' ____  ', '/    \\ ', '| /\ | ', '| || | ', '| \/ | ', '\____/ ']
+
+    #Misc
+    
+    for letter in user:
+      if letter == " ":
+        for i in range (len (space)):
+          output [i] += space [i]
+  
+    
+      #Numbers
+  
+
+
+    
+      if letter == "1":
+        for i in range (len (one)):
+          output [i] += one [i]
+
+      if letter == "2":
+        for i in range (len (two)):
+          output [i] += two [i]
+
+      if letter == "3":
+        for i in range (len (three)):
+          output [i] += three [i]
+
+      if letter == "4":
+        for i in range (len (four)):
+          output [i] += four [i]
+
+      if letter == "5":
+        for i in range (len (five)):
+          output [i] += five [i]
+
+      if letter == "6":
+        for i in range (len (six)):
+          output [i] += six [i]
+
+      if letter == "7":
+        for i in range (len (seven)):
+          output [i] += seven [i]
+
+      if letter == "8":
+        for i in range (len (eight)):
+          output [i] += eight [i]
+
+      if letter == "9":
+        for i in range (len (nine)):
+          output [i] += nine [i]
+
+      if letter == "0":
+        for i in range (len (zero)):
+          output [i] += zero [i]
+
+      #Letters
+    
+
+
+      if letter == "A":
+        for i in range (len (A)):
+          output [i] += A [i]
+
+      if letter == "a":
+        for i in range (len (A)):
+          output [i] += A [i]
+
+      if letter == "B":
+        for i in range (len (B)):
+          output [i] += B [i]
+
+      if letter == "b":
+        for i in range (len (B)):
+          output [i] += B [i]
+
+      if letter == "C":
+        for i in range (len (C)):
+          output [i] += C [i]
+
+      if letter == "c":
+        for i in range (len (C)):
+          output [i] += C [i]
+
+      if letter == "D":
+        for i in range (len (D)):
+          output [i] += D [i]
+
+      if letter == "d":
+        for i in range (len (D)):
+          output [i] += D [i]
+
+      if letter == "E":
+        for i in range (len (E)):
+          output [i] += E [i]
+
+      if letter == "e":
+        for i in range (len (E)):
+          output [i] += E [i]
+
+      if letter == "F":
+        for i in range (len (F)):
+          output [i] += F [i]
+
+      if letter == "f":
+        for i in range (len (F)):
+          output [i] += F [i]
+
+      if letter == "G":
+        for i in range (len (G)):
+          output [i] += G [i]
+
+      if letter == "g":
+        for i in range (len (G)):
+          output [i] += G [i]
+
+      if letter == "H":
+        for i in range (len (H)):
+          output [i] += H [i]
+
+      if letter == "h":
+        for i in range (len (H)):
+          output [i] += H [i]
+
+      if letter == "I":
+        for i in range (len (I)):
+          output [i] += I [i]
+
+      if letter == "i":
+        for i in range (len (I)):
+          output [i] += I [i]
+
+      if letter == "J":
+        for i in range (len (J)):
+          output [i] += J [i]
+
+      if letter == "j":
+        for i in range (len (J)):
+          output [i] += J [i]
+
+      if letter == "K":
+        for i in range (len (K)):
+          output [i] += K [i]
+
+      if letter == "k":
+        for i in range (len (K)):
+          output [i] += K [i]
+
+      if letter == "L":
+        for i in range (len (L)):
+          output [i] += L [i]
+
+      if letter == "l":
+        for i in range (len (L)):
+          output [i] += L [i]
+
+      if letter == "M":
+        for i in range (len (M)):
+          output [i] += M [i]
+
+      if letter == "m":
+        for i in range (len (M)):
+          output [i] += M [i]
+
+      if letter == "N":
+        for i in range (len (N)):
+          output [i] += N [i]
+
+      if letter == "n":
+        for i in range (len (N)):
+          output [i] += N [i]
+
+      if letter == "O":
+        for i in range (len (O)):
+          output [i] += O [i]
+
+      if letter == "o":
+        for i in range (len (O)):
+          output [i] += O [i]
+
+      if letter == "P":
+        for i in range (len (P)):
+          output [i] += P [i]
+
+      if letter == "p":
+        for i in range (len (P)):
+          output [i] += P [i]
+
+      if letter == "Q":
+        for i in range (len (Q)):
+          output [i] += Q [i]
+
+      if letter == "q":
+        for i in range (len (Q)):
+          output [i] += Q [i]
+
+      if letter == "R":
+        for i in range (len (R)):
+          output [i] += R [i]
+
+      if letter == "r":
+        for i in range (len (R)):
+          output [i] += R [i]
+
+      if letter == "S":
+        for i in range (len (S)):
+          output [i] += S [i]
+
+      if letter == "s":
+        for i in range (len (S)):
+          output [i] += S [i]
+
+      if letter == "T":
+        for i in range (len (T)):
+          output [i] += T [i]
+
+      if letter == "t":
+        for i in range (len (T)):
+          output [i] += T [i]
+
+      if letter == "U":
+        for i in range (len (U)):
+          output [i] += U [i]
+
+      if letter == "u":
+        for i in range (len (U)):
+          output [i] += U [i]
+
+      if letter == "V":
+        for i in range (len (V)):
+          output [i] += V [i]
+
+      if letter == "v":
+        for i in range (len (V)):
+          output [i] += V [i]
+
+      if letter == "W":
+        for i in range (len (W)):
+          output [i] += W [i]
+
+      if letter == "w":
+        for i in range (len (W)):
+          output [i] += W [i]
+
+      if letter == "X":
+        for i in range (len (X)):
+          output [i] += X [i]
+
+      if letter == "x":
+        for i in range (len (X)):
+          output [i] += X [i]
+
+      if letter == "Y":
+        for i in range (len (Y)):
+          output [i] += Y [i]
+
+      if letter == "y":
+        for i in range (len (Y)):
+          output [i] += Y [i]
+
+      if letter == "Z":
+        for i in range (len (Z)):
+          output [i] += Z [i]
+
+      if letter == "z":
+        for i in range (len (Z)):
+          output [i] += Z [i]
+
+      #Symbols
+    
+      if letter == "!":
+        for i in range (len (s1)):
+          output [i] += s1 [i]
+
+      if letter == "@":
+        for i in range (len (s2)):
+          output [i] += s2 [i]
+
+      if letter == "#":
+        for i in range (len (s3)):
+          output [i] += s3 [i]
+
+      if letter == "$":
+        for i in range (len (s4)):
+          output [i] += s4 [i]
+      
+      if letter == "%":
+        for i in range (len (s5)):
+          output [i] += s5 [i]
+
+      if letter == "^":
+        for i in range (len (s6)):
+          output [i] += s6 [i]
+
+      if letter == "&":
+        for i in range (len (s7)):
+          output [i] += s7 [i]
+
+      if letter == "*":
+        for i in range (len (s8)):
+          output [i] += s8 [i]
+
+      if letter == "(":
+        for i in range (len (s9)):
+          output [i] += s9 [i]
+
+      if letter == ")":
+        for i in range (len (s0)):
+          output [i] += s0 [i]
+
+      if letter == "`":
+        for i in range (len (s11)):
+          output [i] += s11 [i]
+
+      if letter == "~":
+        for i in range (len (s12)):
+          output [i] += s12 [i]
+
+      if letter == "-":
+        for i in range (len (s13)):
+          output [i] += s13 [i]
+
+      if letter == "_":
+        for i in range (len (s14)):
+          output [i] += s14 [i]
+
+      if letter == "=":
+        for i in range (len (s15)):
+          output [i] += s15 [i]
+
+      if letter == "+":
+        for i in range (len (s16)):
+          output [i] += s16 [i]
+
+      if letter == "[":
+        for i in range (len (s17)):
+          output [i] += s17 [i]
+
+      if letter == "{":
+        for i in range (len (s18)):
+          output [i] += s18 [i]
+
+      if letter == "]":
+        for i in range (len (s19)):
+          output [i] += s19 [i]
+
+      if letter == "}":
+        for i in range (len (s20)):
+          output [i] += s20 [i]
+
+      if letter == "\\":
+        for i in range (len (s21)):
+          output [i] += s21 [i]
+
+      if letter == "|":
+        for i in range (len (s22)):
+          output [i] += s22 [i]
+
+      if letter == ";":
+        for i in range (len (s23)):
+          output [i] += s23 [i]
+
+      if letter == ":":
+        for i in range (len (s24)):
+          output [i] += s24 [i]
+
+      if letter == "'":
+        for i in range (len (s25)):
+          output [i] += s25 [i]
+
+      if letter == "\"":
+        for i in range (len (s26)):
+          output [i] += s26 [i]
+
+      if letter == ",":
+        for i in range (len (s27)):
+          output [i] += s27 [i]
+
+      if letter == "<":
+        for i in range (len (s28)):
+          output [i] += s28 [i]
+
+      if letter == ".":
+        for i in range (len (s29)):
+          output [i] += s29 [i]
+
+      if letter == ">":
+        for i in range (len (s30)):
+          output [i] += s30 [i]
+
+      if letter == "/":
+        for i in range (len (s31)):
+          output [i] += s31 [i]
+
+      if letter == "?":
+        for i in range (len (s32)):
+          output [i] += s32 [i]
+
+    
+    #Pics
+
+    
+    #End
+    while text == "Press Enter":
+      text = "Type Here: "
+    v1 = v1 + 1
+    #if :
+      #else:
+        #print ("ERROR")
+    #test = 1
+  
+    for out in output:
+      print (out)
+
+#(PDM)

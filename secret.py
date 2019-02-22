cc0ae87d5246ecfc6b5fc692b6d734c155073f1c
IsSomthingWrong <43751371+IsSomthingWrong@users.noreply.github.com>
2019-02-22 16:45:32 -0600

    Create secret.py

---
+++ secret.py
@@ -0,0 +1,101 @@
+def type_secret(text):
+  print ("")
+  print (" ----- ||====   __   ||===\\\\ ||==== ======")
+  print ("//     ||     //  \\\\ ||   || ||       ||  ")
+  print ("\\\\___  ||__   ||     ||===// ||__     ||  ")
+  print ("    \\\\ ||     ||     ||  \\\\  ||       ||  ")
+  print ("    // ||     ||     ||   \\\\ ||       ||  ")
+  print ("-----  ||==== \\\\__// ||   || ||====   ||  ")
+  print ("")
+  print ("_    _   __   ____   ||====")
+  print ("|\\  /|  /  \\  ||  \\\\ ||    ")
+  print ("|\\\\//| / -- \\ ||  || ||__  ")
+  print ("| || | | || | ||  || ||    ")
+  print ("| || | \\ -- / ||  || ||    ")
+  print ("| || |  \\__/  ||__// ||====")
+  print ("")
+  test = 0
+  text = "Type Secret Here: "
+  while 1 > test:
+    user = input (text)
+    output = ["","","","","",""]
+
+    space = ['       ', '       ', '       ', '       ', '       ', '       ']
+    Toaste = [" ----- ||==== ||     ||====   __   ======         ----- ======  ____  ||===\\\\ ======","//     ||     ||     ||     //  \\\\   ||          //       ||   //  \\\\ ||   ||   ||","\\\\___  ||__   ||     ||__   ||       ||          \\\\___    ||   ||  || ||===//   ||","    \\\\ ||     ||     ||     ||       ||              \\\\   ||   ||  || ||  \\\\    ||","    // ||     ||     ||     ||       ||              //   ||   ||==|| ||   \\\\   ||","-----  ||==== \\\\==== ||==== \\\\__//   ||          -----    ||   ||  || ||   ||   ||   For Mr.Earley"]
+
+    b1 = ["WE           ","DID          ","A            ","SECRET       ","             ","For Mr.Loomis"]
+
+    YT = ["   /\\   ","  //\\\\  "," //  \\\\ "," \\\\  // ","  \\\\//  ","   \\/   -https://www.youtube.com/channel/UCYFDSvlTG8egjWYI7jSCWYw"]
+
+
+    
+    
+    if user == "bob":
+      for i in range (len (b1)):
+        output [i] += b1 [i]
+
+    if user == "^^vv<><>ba":
+      for i in range (len (Toaste)):
+        output [i] += Toaste [i]
+
+    if user == "^^Vv<><>ba":
+      for i in range (len (Toaste)):
+        output [i] += Toaste [i]
+
+    if user == "^^vV<><>ba":
+      for i in range (len (Toaste)):
+        output [i] += Toaste [i]
+    
+    if user == "^^VV<><>ba":
+      for i in range (len (Toaste)):
+        output [i] += Toaste [i]
+
+    if user == "^^vv<><>Ba":
+      for i in range (len (Toaste)):
+        output [i] += Toaste [i]
+
+    if user == "^^vv<><>bA":
+      for i in range (len (Toaste)):
+        output [i] += Toaste [i]
+
+    if user == "^^VV<><>Ba":
+      for i in range (len (Toaste)):
+        output [i] += Toaste [i]
+
+    if user == "^^VV<><>bA":
+      for i in range (len (Toaste)):
+        output [i] += Toaste [i]
+
+    if user == "^^vV<><>Ba":
+      for i in range (len (Toaste)):
+        output [i] += Toaste [i]
+
+    if user == "^^vV<><>bA":
+      for i in range (len (Toaste)):
+        output [i] += Toaste [i]
+
+    if user == "^^Vv<><>Ba":
+      for i in range (len (Toaste)):
+        output [i] += Toaste [i]
+
+    if user == "^^Vv<><>bA":
+      for i in range (len (Toaste)):
+        output [i] += Toaste [i]
+
+    if user == "^^vv<><>Ba":
+      for i in range (len (Toaste)):
+        output [i] += Toaste [i]
+
+    if user == "^^vv<><>bA":
+      for i in range (len (Toaste)):
+        output [i] += Toaste [i]
+
+    if user == "TDK":
+      for i in range (len (YT)):
+        output [i] += YT [i]
+      
+
+      
+
+    for out in output:
+      print (out)
